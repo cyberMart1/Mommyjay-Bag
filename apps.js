945082f744a59130ceea65fc2b75bb1d4ad3b6f1
MartinsAde <martadeen01@gmail.com>
2023-02-02 05:51:12 +0100

    added login page

--- apps.js
+++ apps.js
@@ -1,14 +1,31 @@
+//nav bar part
 const bar = document.getElementById('bar')
-const cancle = document.getElementsByClassName('cancle')
+const close = document.querySelector('.close')
 const nav = document.getElementById('navbar')
 
 if(bar) {
     bar.addEventListener('click', ()=> {
         nav.classList.add('active')
-    })
+    });
 }
-if(cancle) {
-    cancle.addEventListener('click', ()=> {
+
+if(close) {
+    close.addEventListener('click', ()=> {
         nav.classList.remove('active')
-    })
+    });
+}
+
+//login part
+const userEl = document.getElementById('username')
+const passEl = document.getElementById('password')
+const btnlogin = document.getElementById('btnlogin')
+
+
+function login(){
+    if(userEl.value === "admin" && passEl.value === "admin") {
+        location.replace("index.html");
+        console.log('works')
+    }else {
+        alert("Wrong Username or Password")
+    }
 }
