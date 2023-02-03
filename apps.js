b229fd02182d3e44486ab4e49764ca4d1a60283d
MartinsAde <martadeen01@gmail.com>
2023-02-03 05:00:10 +0100

    addded bag details and move functions

--- apps.js
+++ apps.js
@@ -5,27 +5,37 @@ const nav = document.getElementById('navbar')
 
 if(bar) {
     bar.addEventListener('click', ()=> {
-        nav.classList.add('active')
+        nav.classList.add('active');
     });
 }
 
 if(close) {
     close.addEventListener('click', ()=> {
-        nav.classList.remove('active')
+        nav.classList.remove('active');
     });
 }
 
-//login part
+//login page
 const userEl = document.getElementById('username')
 const passEl = document.getElementById('password')
-const btnlogin = document.getElementById('btnlogin')
 
 
-function login(){
+function signIn(){
     if(userEl.value === "admin" && passEl.value === "admin") {
         location.replace("index.html");
-        console.log('works')
+        alert("logged in");
+        console.log('works');
     }else {
-        alert("Wrong Username or Password")
+        alert("Wrong Username or Password");
     }
+}
+
+// move from product img to product details
+
+function moveToDetails() {
+    location.replace('bagdetails.html')
+}
+
+function moveToDetails2() {
+    location.replace('bagdetails2.html')
 }
